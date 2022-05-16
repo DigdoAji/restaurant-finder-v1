f798629e6468badb3c38c61795b24020b525d932
DigdoAji <1910817310001@mhs.ulm.ac.id>
2022-05-16 10:11:41 +0800

    Fixing Coding

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
